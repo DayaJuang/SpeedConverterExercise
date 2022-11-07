12a8e76d82a774c828e68b200cbc45482592e643
daya <dayajuangmahaputra@gmail.com>
2022-11-07 19:04:34 +0900

    test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,20 @@
 package org.example;
 
 public class Main {
+    static final double MILE_TO_KILO = 1.609;
     public static void main(String[] args) {
         double[] testInput = {1.5, 10.25, -5.6, 25.42, 75.114};
 
         for (double v : testInput) {
             printConversion(v);
         }
-
     }
 
     public static long toMilesPerHour(double kilometersPerHour){
-        var KILO_TO_MILE = 1.609;
-        long conversion = Math.round(kilometersPerHour / KILO_TO_MILE);
+        // Convert km/h to mph
+        long conversion = Math.round(kilometersPerHour / MILE_TO_KILO);
 
+        // If input is negative, just return -1
         if(kilometersPerHour < 0)
             conversion = -1;
 
@@ -21,8 +22,10 @@ public class Main {
     }
 
     public static void printConversion(double kilometersPerHour){
+        // Convert the input
         long result = toMilesPerHour(kilometersPerHour);
 
+        // Check if the result is valid or not
         if(result == -1)
             System.out.println("Invalid Value");
         else
